5345bd6b4d83b8ef2f9b53d3cf654bb5d9bfe52d
Dejan Aksovic <imdejan00@gmail.com>
2024-01-24 08:32:32 +0100

    Just kill myself

--- backend/api/utilities.py
+++ backend/api/utilities.py
@@ -6,20 +6,18 @@ def format_map(map_name: str):
    file_path = os.path.join(base_path, 'api')
    file_path = os.path.join(file_path, 'maps')
    file_path = os.path.join(file_path, f'{map_name}.txt')
-   values = []
 
    with open(file_path) as f:
       lines = f.readlines()
 
-   for line in lines:
-      new_line = line.replace('\n', '')
-      chars = new_line.split(',')
-      chars.append(0)
-      chars.pop(0)
-      chars.pop(0)
-      values.append(chars)
+   values = np.zeros((len(lines), len(lines)))
 
-   for value in values:
-      for itter, single_value in enumerate(value):
-         value[itter] = int(single_value)
+   for linIndex, line in enumerate(lines):
+      line = line.replace('\n', '')
+      values[linIndex][linIndex] = 0
+      for valueIndex, value in enumerate(line.split(',')):
+         if valueIndex > 1 and valueIndex < 5:
+            values[linIndex-2][valueIndex] = int(value)
+            values[valueIndex][linIndex - 2] = int(value)
+
    return values
